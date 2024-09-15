1070ddd1704f21065f8da2cfc8b05db66b8867a1
mokhs00 <mokhs00@naver.com>
2024-09-15 21:37:37 +0900

    Update default port to 8080

--- pkg/fx/fiberfx/config.go
+++ pkg/fx/fiberfx/config.go
@@ -1,11 +1,11 @@
 package fiberfx
 
 type Config struct {
-	Port int `envconfig:"PORT" default:"3000"`
+	Port int `envconfig:"PORT" default:"8080"`
 }
 
 func parseConfig() *Config {
 	return &Config{
-		Port: 3000,
+		Port: 8080,
 	}
 }
