6a4419aa32d6caf49590da3d0f71b44fe0ae3ca4
falconlee236 <falconlee236@cau.ac.kr>
2024-09-20 16:25:29 +0900

    Update insert_mock_data.sql: add insert script to use mock data

---
+++ ddl/insert_mock_data.sql
@@ -0,0 +1,228 @@
+INSERT INTO restaurant(restaurant_id,name,address,description,maximum_price_per_person,minimum_price_per_person,latitude,longitude,business_hours_json,recommendation_score,average_score_from_naver,total_review_count_from_naver,average_score_from_kakao,total_review_count_from_kakao,total_review_count) VALUES (1,'농민백암순대 본점','서울특별시 강남구 선릉로86길 40-4','다코숨은맛집'', ''수요미식회'', ''혼밥'', ''식사모임''',13000,10000,127.0530139,37.5037,'[
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SUNDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_MONDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_TUESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_WEDNESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_THURSDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_FRIDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SATURDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        }
+]',99,4.6,50,4.6,53,103);
+INSERT INTO restaurant(restaurant_id,name,address,description,maximum_price_per_person,minimum_price_per_person,latitude,longitude,business_hours_json,recommendation_score,average_score_from_naver,total_review_count_from_naver,average_score_from_kakao,total_review_count_from_kakao,total_review_count) VALUES (2,'뽕나무쟁이 선릉본점','서울특별시 강남구 역삼로65길 31','vj특공대'', ''술모임'', ''회식''',12000,10000,37.5032108,127.0520516,'[
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SUNDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_MONDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_TUESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_WEDNESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_THURSDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_FRIDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SATURDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        }
+]',98,4.2,30,4.2,33,63);
+INSERT INTO restaurant(restaurant_id,name,address,description,maximum_price_per_person,minimum_price_per_person,latitude,longitude,business_hours_json,recommendation_score,average_score_from_naver,total_review_count_from_naver,average_score_from_kakao,total_review_count_from_kakao,total_review_count) VALUES (3,'장정 정','서울특별시 강남구 선릉로82길 15 1층','혼밥'', ''회식''',14000,10000,37.502822,127.0512635,'[
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SUNDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_MONDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_TUESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_WEDNESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_THURSDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_FRIDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SATURDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        }
+]',98,4.1,20,4.1,20,40);
+INSERT INTO restaurant(restaurant_id,name,address,description,maximum_price_per_person,minimum_price_per_person,latitude,longitude,business_hours_json,recommendation_score,average_score_from_naver,total_review_count_from_naver,average_score_from_kakao,total_review_count_from_kakao,total_review_count) VALUES (4,'신동궁 감자탕 선릉직영점','서울특별시 강남구 선릉로86길 39 1층 신동궁감자탕 뼈숯불구이','술모임'', ''회식''',15000,10000,37.5040863,127.0529113,'[
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SUNDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_MONDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_TUESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_WEDNESDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_THURSDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_FRIDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        },
+        {
+            "dayOfWeek": "DAY_OF_WEEK_SATURDAY",
+            "openTime": "10:00",
+            "closingTime": "20:00",
+            "isClosedDay": false
+        }
+]',98,4.4,20,4.4,29,49);
+
+
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'국밥',10000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'토종순대',13000,'토종순대','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'모둠 수육',36000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'국밥 특',12000,'국밥 특','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'국밥 정식',15000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'술국',19000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (1,'오소리감투',18000,'오소리감투','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'국밥',10000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'토종순대',13000,'토종순대','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'모둠 수육',36000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'국밥 특',12000,'국밥 특','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'국밥 정식',15000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'술국',19000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (2,'오소리감투',18000,'오소리감투','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'국밥',10000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'토종순대',13000,'토종순대','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'모둠 수육',36000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'국밥 특',12000,'국밥 특','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'국밥 정식',15000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'술국',19000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (3,'오소리감투',18000,'오소리감투','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'국밥',10000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'토종순대',13000,'토종순대','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'모둠 수육',36000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'국밥 특',12000,'국밥 특','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'국밥 정식',15000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'술국',19000,'','');
+INSERT INTO restaurant_menu(restaurant_id,name,price,description,image_url) VALUES (4,'오소리감투',18000,'오소리감투','');
+
+
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (1,'이명진',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (1,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (1,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (1,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (2,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (2,'이명진',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (2,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (2,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (2,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (3,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (3,'이명진',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (3,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (3,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (3,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (4,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (4,'이명진',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (4,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (4,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (4,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
+INSERT INTO restaurant_review(restaurant_id,writer_name,score,content,wrote_at) VALUES (4,'김상은',4.5,'리뷰리뷰','2024-09-15 17:00:10');
