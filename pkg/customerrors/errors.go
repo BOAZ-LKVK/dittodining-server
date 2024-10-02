2d9e2b2ae30dc6f9f2a9c4634d2d6cd67a4bbeeb
김한수(Hansu Kim) <mokhs00@naver.com>
2024-10-02 19:36:30 +0900

    Add 공통 application error (#10)

--- pkg/customerrors/errors.go
+++ pkg/customerrors/errors.go
@@ -1,1 +1,12 @@
 package customerrors
+
+var _ error = &ApplicationError{}
+
+type ApplicationError struct {
+	Code int
+	Err  error
+}
+
+func (e *ApplicationError) Error() string {
+	return e.Err.Error()
+}
